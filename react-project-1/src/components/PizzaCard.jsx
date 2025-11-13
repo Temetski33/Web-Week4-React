77621cef3d80fb741729e66cac6ec22b15484885
Temetski33 <teemu.laasio@gmail.com>
2025-11-13 13:35:38 +0200

    Edit pizzacards

--- react-project-1/src/components/PizzaCard.jsx
+++ react-project-1/src/components/PizzaCard.jsx
@@ -1,11 +1,16 @@
-export const PizzaCard = () => {
+//import pizzaImg from './assets/images/Pizzazaza.jpg';
+
+const PizzaCard = ({pizza}) => {
   return (
     <>
       <div className="card">
-        <h3>Pizza</h3>
+        <h3>{pizza.name}</h3>
         <div>pizza info</div>
+        <div>Hinta {pizza.price}</div>
+        <img src={pizzaImg} alt="" srcSet=""></img>
       </div>
     </>
+  );
+};
 
-  )
-}
+export default PizzaCard;
--- react-project-1/src/components/PizzaMenu.jsx
+++ react-project-1/src/components/PizzaMenu.jsx
@@ -1,13 +1,21 @@
 import { PizzaCard } from "./PizzaCard"
 
+const pizzas = [
+  {id: 1, name: 'Margarita', price: 12},
+  {id: 2, name: 'El Giga Pollo', price: 666},
+  {id: 3, name: 'Vegetariana', price: 15},
+];
+
 export const PizzaMenu = () => {
   return (
     <>
       <h2>PizzaMenu</h2>
       <div>Pizza timeeeeeeeeee!</div>
-      <PizzaCard />
-      <PizzaCard />
-      <PizzaCard />
+      <div className='container'>{
+        pizzas.map((pizza) => {
+          <PizzaCard key={pizza.id} pizza={pizza} />
+        })}
+      </div>
     </>
 
   )
